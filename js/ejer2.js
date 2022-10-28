82073301f820ff9bfa9c80ff60797de6d52c9a2e
Daniel Silva <danielsilvavalencia25@gmail.com>
2022-10-27 23:02:50 -0500

    ej15 agregado

--- js/ejer2.js
+++ js/ejer2.js
@@ -1,6 +1,6 @@
 function callMenu(){
     let nro_ejercicio= parseInt(
-        prompt("ingrese el numero de ejercicio que quiere ejecutar: \r\n 1.suma. \r\n 2.Promedio de exmamenes \r\n 3.Calcular el area de un rectangulo \r\n 4.Calcular el area de un triangulo \r\n 5.Calcular el area de una circunferencia \r\n 6.Calcular el sueldo semanal de un trabajador. \r\n 7.Convertir metros a pulgadas \r\n 8.Convertir soles a dolares \r\n 9.Determinar edad de persona \r\n 10.Determinar persona de menor edad. \r\n 11.Determinar bono de trabajador. \r\n 12. Determinar Salaio de 6 años de un profesor" )
+        prompt("ingrese el numero de ejercicio que quiere ejecutar: \r\n 1.suma. \r\n 2.Promedio de exmamenes \r\n 3.Calcular el area de un rectangulo \r\n 4.Calcular el area de un triangulo \r\n 5.Calcular el area de una circunferencia \r\n 6.Calcular el sueldo semanal de un trabajador. \r\n 7.Convertir metros a pulgadas \r\n 8.Convertir soles a dolares \r\n 9.Determinar edad de persona \r\n 10.Determinar persona de menor edad. \r\n 11.Determinar bono de trabajador. \r\n 12. Determinar Salaio de 6 años de un profesor \r\n 15. Determinar edad para votar")
     );
     if(isNaN(nro_ejercicio)){
         alert ("hey!! por fvor ingresa valores ")
@@ -66,6 +66,9 @@ function MenuEjercicios(nro_ejercicio){
         case 12:
             const pSalario = parseInt(prompt("Ingrese salario"));
             alert(ej12_salarioProfesor(pSalario));
+        case 15:
+            const edadV = parseInt(prompt("Ingresar edad: "));
+            alert(ej15_edadVotar(edadV));
     }
 }
 
@@ -170,7 +173,6 @@ function ej11_bonoTrabajo(añoT){
         }
     }
 }
-
 function ej12_salarioProfesor (pSalario){
     let sal =0;
     let cantA;
@@ -190,5 +192,16 @@ function ej12_salarioProfesor (pSalario){
          return "el salrio en los 6 años es:$"+pSalario;
     }
 }
-
-
+function ej15_edadVotar(edadV){
+    if(isNaN(edadV)){
+        return "Porfavor poner valores";
+    } else {
+        if(edadV >= 0 && edadV < 18){
+            return "Es menor de edad, No puede votar"
+        } else if(edadV >= 18 && edadV < 65){
+            return "Es mayor de edad, si puede votar"
+        } else if(edadV > 65){
+            return "Es mayor pero es opcional votar"
+        }
+    }
+}
